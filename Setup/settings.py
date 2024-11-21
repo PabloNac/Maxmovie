1072ef04ad330e78061ee253272543ca8fded406
Pablo Policarpo <pablopolicarpo03@gmail.com>
2024-11-21 10:57:49 -0300

    add: App usuarios, templates: Login.html e Register.html, respectivas views e urls em Setup

--- Setup/settings.py
+++ Setup/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "Home.apps.HomeConfig",
+    "Usuarios.apps.UsuariosConfig",
 ]
 
 MIDDLEWARE = [
--- Setup/urls.py
+++ Setup/urls.py
@@ -2,5 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from Home.views import home
+from Usuarios.views import Tela_login, Tela_register
 
-urlpatterns = [path("admin/", admin.site.urls), path("", home)]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", home),
+    path("Login/", Tela_login),
+    path("Registrar-se/", Tela_register),
+]
