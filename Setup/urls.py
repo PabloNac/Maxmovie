e19c3b96bbef2d0f398fb6e71c8ee02e84eaa768
Pablo Policarpo <pablopolicarpo03@gmail.com>
2024-11-22 11:13:34 -0300

    refactor: Atualizando as views das urls

--- Setup/urls.py
+++ Setup/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 
 from Home.views import home
-from Usuarios.views import Tela_login, Tela_register
+from Usuarios.views import Tela_Login, Tela_Register
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", home, name="home"),
-    path("Login/", Tela_login, name="Tela_Login"),
-    path("Registrar-se/", Tela_register, nome="Tela_Register"),
+    path("", home.as_view(), name="home"),
+    path("Login/", Tela_Login.as_view(), name="Tela_Login"),
+    path("Registrar-se/", Tela_Register.as_view(), name="Tela_Register"),
 ]
