b008f034f176f32a227396ace49612aea95547a0
Pablo <pablopolicarpo03@gmail.com>
2024-11-26 09:09:01 -0300

    ref: Refatorando a classe Login para Register

--- Usuarios/models.py
+++ Usuarios/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+##Refatorando Login para Register
+class Register(models.Model):
+    __name__="Register"
+    Usuario = models.CharField(max_length=50, null=False, blank=False)
+    Email = models.EmailField(max_length=254, null=False, blank=False)
+    Senha = models.CharField(max_length=50, null=False, blank=False)
+ 
